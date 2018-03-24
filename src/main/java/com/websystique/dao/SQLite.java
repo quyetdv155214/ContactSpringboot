44e0b57b2736dfb83544ffb22d5a5d7424936491
Tyeuq <quyetdv15524@gmail.com>
2018-03-25 02:41:49 +0700

    single main class accepted

--- src/main/java/com/websystique/dao/SQLite.java
+++ src/main/java/com/websystique/dao/SQLite.java
@@ -22,53 +22,53 @@ public class SQLite {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        SQLiteManager manager = new SQLiteManager();
-        try {
-
-            //   GET ACCOUNT TOKEN
-//            String token = manager.getAccountToken("id", "password");
+//    public static void main(String[] args) {
+//        // TODO code application logic here
+//        SQLiteManager manager = new SQLiteManager();
+//        try {
+//
+//            //   GET ACCOUNT TOKEN
+////            String token = manager.getAccountToken("id", "password");
+////            System.out.println(token);
+//            // ADD NEW CONTACT
+////            Contact c = new Contact("Au duy", "+84 965 113300", "duyakse04298@fpt.edu.vn", "FPT University");
+////            boolean added = manager.addContactWithToken(token, c);
+////            System.out.println(added);
+//            // EDIT CONTACT
+////            Contact c = new Contact(0,"id","Au duy ", "+84 965 113300", "duyakse04298@fpt.edu", "FPT University");
+////            boolean added = manager.editContactWithToken(token, c);
+////            System.out.println(added);
+//            // REGISTER
+////            boolean register = manager.registerAccount("auduy", "123456");
+////            System.out.println(register);
+//
+//            //GET ACCOUNT TOKEN
+//            String token = manager.getAccountToken("auduy", "123456");
 //            System.out.println(token);
-            // ADD NEW CONTACT
-//            Contact c = new Contact("Au duy", "+84 965 113300", "duyakse04298@fpt.edu.vn", "FPT University");
-//            boolean added = manager.addContactWithToken(token, c);
-//            System.out.println(added);
-            // EDIT CONTACT
-//            Contact c = new Contact(0,"id","Au duy ", "+84 965 113300", "duyakse04298@fpt.edu", "FPT University");
-//            boolean added = manager.editContactWithToken(token, c);
-//            System.out.println(added);
-            // REGISTER
-//            boolean register = manager.registerAccount("auduy", "123456");
-//            System.out.println(register);
-
-            //GET ACCOUNT TOKEN
-            String token = manager.getAccountToken("auduy", "123456");
-            System.out.println(token);
-            
-            
-            // GET ALL CONTACT WITH TOKEN
-            List<Contact> contacts = manager.getAllContactWithToken(token);
-            for (Contact contact : contacts) {
-                System.out.println(contact.toString());
-            }
-            //DELETE CONTACT WITH TOKEN
-            if (!contacts.isEmpty()) {
-                boolean delete = manager.deleteWithToken(token, contacts.get(0).getContactId());
-                System.out.println(delete + "delete");
-            }
-            
-            
-            // GET ALL CONTACT WITH TOKEN AGAIN
-            List<Contact> contacts2 = manager.getAllContactWithToken(token);
-            System.out.println("list 2 below:");
-            for (Contact contact : contacts2) {
-                System.out.println(contact.toString());
-            }
-
-        } catch (SQLException ex) {
-            Logger.getLogger(SQLite.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
+//            
+//            
+//            // GET ALL CONTACT WITH TOKEN
+//            List<Contact> contacts = manager.getAllContactWithToken(token);
+//            for (Contact contact : contacts) {
+//                System.out.println(contact.toString());
+//            }
+//            //DELETE CONTACT WITH TOKEN
+//            if (!contacts.isEmpty()) {
+//                boolean delete = manager.deleteWithToken(token, contacts.get(0).getContactId());
+//                System.out.println(delete + "delete");
+//            }
+//            
+//            
+//            // GET ALL CONTACT WITH TOKEN AGAIN
+//            List<Contact> contacts2 = manager.getAllContactWithToken(token);
+//            System.out.println("list 2 below:");
+//            for (Contact contact : contacts2) {
+//                System.out.println(contact.toString());
+//            }
+//
+//        } catch (SQLException ex) {
+//            Logger.getLogger(SQLite.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+//    }
 
 }
